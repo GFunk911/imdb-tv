00af7cb7e55b289593cea0ed6a5cd3d883211f91
Mike Harris <GFunk913@gmail.com>
2009-02-19 09:49:13 -0500

    Initial commit to imdb-tv.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'imdb_tv'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'imdb_tv'
+
+Spec::Runner.configure do |config|
+  
+end
